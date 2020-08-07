005ff09bee7bc269ba06f76e63226756225762a4
Tran Van Thuc <tranvanthucduytan@gmail.com>
2020-08-07 21:55:31 +0700

    Lesson 2: Import Bootstrap

---
+++ resources/views/list-post.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('title')
+    List Posts
+@endsection
+
+@section('head')
+    <link rel="stylesheet" href={{ asset('css/list-post.css') }}>
+@endsection
+
+@section('content')
+    <h3 class="text-danger">This is list posts</h3>
+@endsection
